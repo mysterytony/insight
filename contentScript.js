9cd3b06f6d0fc95fb2bcade032de192199e00a4e
mysterytony <lihenantony1997@gmail.com>
2018-02-25 01:02:04 -0500

    mvp

--- contentScript.js
+++ contentScript.js
@@ -3,3 +3,13 @@ chrome.runtime.sendMessage({
     source: document.documentElement.innerText
 });
 
+var insights = null;
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action == "insightResults") {
+        insights = request.source;
+    }
+    else if (request.action == "requestInsight") {
+        sendResponse(insights);
+    }
+});
